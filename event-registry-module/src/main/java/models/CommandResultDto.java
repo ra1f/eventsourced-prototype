ae3b2a921eb0ab008acab4758195428628505e86
ra1f <ralf.dueerkop@gmail.com>
2015-03-30 18:09:37 +0200

    docker postgres hell

---
+++ event-registry-module/src/main/java/models/CommandResultDto.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Created by dueerkopra on 30.03.2015.
+ */
+public class CommandResultDto {
+
+  private int commandsAdded;
+
+  public int getCommandsAdded() {
+    return commandsAdded;
+  }
+
+  public void setCommandsAdded(int commandsAdded) {
+    this.commandsAdded = commandsAdded;
+  }
+}
--- postgres/create.sql
+++ postgres/create.sql
@@ -1,18 +1,20 @@
-CREATE TABLE eventlog {
-  id bigint NOT NULL,
-  event varchar(80) NOT NULL,
-  animalId varchar(80) NOT NULL,
-  occurence timestamp NOT NULL
-  PRIMARY KEY(id)
-}
+CREATE DATABASE eventsourced;
 
-CREATE UNIQUE INDEX occurence_idx ON eventlog (occurence);
+CREATE TABLE eventsourced.eventlog (
+  id bigint NOT NULL,\
+  event varchar(80) NOT NULL,\
+  animalId varchar(80) NOT NULL,\
+  occurence timestamp NOT NULL\
+  PRIMARY KEY(id)\
+);
 
-CREATE TABLE zoo {
-  animalId varchar(80) NOT NULL,
-  last_occurence timestamp NOT NULL,
-  feeling_of_satiety varchar(40),
-  mindstate varchar(40),
-  hygiene varchar(40),
-  PRIMARY KEY (animalId)
-}
+CREATE UNIQUE INDEX eventsourced.occurence_idx ON eventsourced.eventlog (occurence);
+
+CREATE TABLE eventsourced.zoo (\
+  animalId varchar(80) NOT NULL,\
+  last_occurence timestamp NOT NULL,\
+  feeling_of_satiety varchar(40),\
+  mindstate varchar(40),\
+  hygiene varchar(40),\
+  PRIMARY KEY (animalId)\
+);
