7644b77b3d2110d94755af08459c8adc15eac135
ra1f <ralf.dueerkop@gmail.com>
2015-04-10 14:44:14 +0200

    removed old module

--- event-registry-module/src/main/java/zoo/common/AnimalId.java
+++
@@ -1,8 +0,0 @@
-package zoo.common;
-
-/**
- * Created by dueerkopra on 27.03.2015.
- */
-public enum AnimalId {
-  Lions, Monkeys, Elephants
-}
--- event-registry-module/src/main/java/zoo/common/Command.java
+++
@@ -1,8 +0,0 @@
-package zoo.common;
-
-/**
- * Created by dueerkopra on 27.03.2015.
- */
-public enum Command {
-  Digest, Feed, Sadden, Play, MessUp, CleanUp
-}
