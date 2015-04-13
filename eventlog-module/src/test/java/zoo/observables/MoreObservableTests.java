2a78f6fce3d50d0c344bbe3db1ffe77a25ad801b
ra1f <ralf.dueerkop@gmail.com>
2015-04-13 17:44:28 +0200

    * added some prove of concept tests

---
+++ eventlog-module/src/test/java/zoo/observables/MoreObservableTests.java
@@ -0,0 +1,54 @@
+package zoo.observables;
+
+import rx.Observable;
+import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
+
+/**
+ * Created by dueerkopra on 13.04.2015.
+ */
+public class MoreObservableTests {
+
+
+
+  public static void main(String[] args) throws Exception {
+
+    PublishSubject<String> publishSubject = PublishSubject.create();
+
+    Observable<String> observable = publishSubject.observeOn(Schedulers.newThread());
+
+    observable.subscribe(m -> {
+      Long id = Thread.currentThread().getId();
+      if (m.equals("Yo")) {
+        System.out.println(String.format("%d-yo: I know you, %s.", id, m));
+      } else {
+        System.out.println(String.format("%d-yo: I don't know you, %s. Go away.", id, m));
+      }
+    });
+
+    observable.subscribe(m -> {
+      Long id = Thread.currentThread().getId();
+      if (m.equals("Man")) {
+        System.out.println(String.format("%d-man: I know you, %s.", id, m));
+      } else {
+        System.out.println(String.format("%d-man: I don't know you, %s. Go away.", id, m));
+      }
+    });
+
+    observable.subscribe(m -> {
+      Long id = Thread.currentThread().getId();
+      if (m.equals("Yep")) {
+        System.out.println(String.format("%d-yep: I know you, %s.", id, m));
+      } else {
+        System.out.println(String.format("%d-yep: I don't know you, %s. Go away.", id, m));
+      }
+    });
+
+    publishSubject.onNext("Yo");
+    publishSubject.onNext("Man");
+    publishSubject.onNext("Yep");
+
+    Thread.sleep(30000);
+
+  }
+}
---
+++ eventlog-module/src/test/java/zoo/observables/ObservableTest.java
@@ -0,0 +1,32 @@
+package zoo.observables;
+
+/**
+ * Created by dueerkopra on 13.04.2015.
+ */
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(JUnit4.class)
+public class ObservableTest {
+
+  @Test
+  public void blah() throws Exception {
+
+    Observable
+        .interval(2, TimeUnit.SECONDS)
+        .map(x -> {return "Hello World";})
+        .observeOn(Schedulers.newThread())
+        .subscribe(n -> System.out.println(n));
+
+    Thread.sleep(30000);
+
+  }
+
+
+}
